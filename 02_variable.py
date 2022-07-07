52a073d0abe4c1177630b775b3afea0b474f904b
crpoudyal <crpoudyal@gmail.com>
2022-07-07 18:44:25 +0545

    2 ; Variables

---
+++ 02_variable.py
@@ -0,0 +1,26 @@
+# Variables in Python
+
+a = "hello" # <string> variable
+A = 34
+a = "nepal"
+_b = 12 # <integer> variable
+c = 12.89 # <float> variable
+isTrue = True # <Boolean> variable
+isNone = None # <None> variable
+
+#Variables are case_sensitive
+
+print(a)
+print(A)
+print(_b)
+print(c)
+print(isTrue)
+print(isNone)
+
+# Print type of Varibles using built-in type() function
+
+print(type(a))
+print(type(_b))
+print(type(c))
+print(type(isTrue))
+print(type(isNone))
