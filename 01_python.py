6ac2ae347eac2d4d7c130466c024151b3b42ba23
crpoudyal <crpoudyal@gmail.com>
2022-07-07 18:21:33 +0545

    initial commit : comments , print

---
+++ 01_python.py
@@ -0,0 +1,16 @@
+# comments in python
+'''
+Hello this is multiline
+comment
+'''
+#print hello in terminal
+
+print("hello")
+
+#print multiline code in terminal
+
+print('''Hello welcome to
+Python code with
+Multiline code in 
+print function
+''')
