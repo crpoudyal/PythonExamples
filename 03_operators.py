d36035c029f0ae1f502d44fa1b7ff44477f358ce
crpoudyal <crpoudyal@gmail.com>
2022-07-07 19:04:22 +0545

    operators in py

---
+++ 03_operators.py
@@ -0,0 +1,40 @@
+# Arithmetic Operator
+
+a = 10
+b = 12
+
+print("Sum of a and b : ",a+b)
+print("Difference of a and b : ",a-b)
+print("Multiplication of a and b : ",a*b)
+print("Division of a and b : ",a/b)
+
+# Assignment Operator
+
+c = 6 # 6 
+c += 2 # 8
+c -= 1 # 7
+c *= 4 # 28
+c /= 2 # 14
+
+print(c)
+
+# Comparision Operator
+
+comp = (2>3)
+comp_1 = (4<3)
+comp_2 = (4 == 4)
+comp_3 = (2!=3)
+
+print(comp)
+print(comp_1)
+print(comp_2)
+print(comp_3)
+
+# Logical Operator
+
+logic = True
+logic1 = False
+
+print("Value of logic and logic1",(logic and logic1))
+print("Value of logic and logic1",(logic or logic1))
+print("Value of logic and logic1",(not logic))
