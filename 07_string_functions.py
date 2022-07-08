dba181f4d9ac7c2714a38024e6f85f7276ef108e
crpoudyal <crpoudyal@gmail.com>
2022-07-08 12:17:49 +0545

    string fuction in py

---
+++ 07_string_functions.py
@@ -0,0 +1,13 @@
+quote = "your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking."
+
+print(len(quote)) # 156
+
+print(quote.endswith("thinking.")) # True
+
+print(quote.count("o")) # 10 -- counts characters and string as well
+
+print(quote.capitalize()) # Capitalize first letter in sentences
+
+print(quote.find("is")) # "is" lies in 10 index of the above quote (finds first occurance only)
+
+print(quote.replace("living","------")) # replace "living" with "------" (replace all the occurance)
